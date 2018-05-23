6236312fcaff6c8bf82b535f96f9f345b3652b18
John Tavenner <jtavenner@gmail.com>
2018-05-23 13:02:21 -0400

    Add project files.

---
+++ BusinessApplication.Core/BusinessService.cs
@@ -0,0 +1,27 @@
+﻿using System.Configuration;
+
+namespace BusinessApplication.Core
+{
+    public class BusinessService
+    {
+        private readonly string _databaseConnectionString =
+            ConfigurationManager.ConnectionStrings["MyConnectionString"].ConnectionString;
+
+        private readonly string _webServiceAddress = ConfigurationManager.AppSettings["MyWebServiceAddress"];
+        private readonly LoggingDataSink _loggingDataSink;
+
+        private IDataAccessComponent _dataAccessComponent;
+        private IWebServiceProxy _webServiceProxy;
+        private ILoggingComponent _loggingComponent;
+        
+
+        public BusinessService()
+        {
+            _loggingDataSink = new LoggingDataSink();
+            _loggingComponent = new LoggingComponent(_loggingDataSink);
+            _webServiceProxy = new WebServiceProxy(_webServiceAddress);
+            _dataAccessComponent = new DataAccessComponent(_databaseConnectionString);
+        }
+    }
+
+}
