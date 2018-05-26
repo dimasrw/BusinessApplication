14f67fac81f099fd18fdac00f13747e917558bc6
jtavenner <username@users.noreply.github.com>
2018-05-26 15:38:21 -0400

    Refactored away concrete instances

--- BusinessApplication.Core/CoreModule.cs
+++ BusinessApplication.Core/CoreModule.cs
@@ -1,9 +1,11 @@
-﻿using Ninject.Modules;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Ninject.Modules;
+using Ninject.Activation;
+using System.Configuration;
 
 namespace BusinessApplication.Core
 {
@@ -13,6 +15,28 @@ namespace BusinessApplication.Core
         {
             Bind<ILoggingDataSink>().To<LoggingDataSink>();
             Bind<ILoggingComponent>().To<LoggingComponent>();
+            Bind<IDataAccessComponent>().ToProvider(new DataAccessComponentProvider());
+            Bind<IWebServiceProxy>().ToProvider(new WebServiceProxyComponentProvider());
+        }
+    }
+
+    public class DataAccessComponentProvider : Provider<IDataAccessComponent>
+    {
+        protected override IDataAccessComponent CreateInstance(IContext context)
+        {
+            var databaseConnectionString =
+                ConfigurationManager.ConnectionStrings["MyConnectionString"].ConnectionString;
+            return new DataAccessComponent(databaseConnectionString);
+        }
+    }
+
+    public class WebServiceProxyComponentProvider : Provider<IWebServiceProxy>
+    {
+        protected override IWebServiceProxy CreateInstance(IContext context)
+        {
+            var webServiceAddress = ConfigurationManager.AppSettings
+                ["MyWebServiceAddress"];
+            return new WebServiceProxy(webServiceAddress);
         }
     }
 }
