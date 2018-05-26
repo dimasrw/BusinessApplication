829924a039e4c95b16c4447866c8266d83a5a92f
jtavenner <username@users.noreply.github.com>
2018-05-26 17:04:07 -0400

    Moved the PersonService tests to their own class and used Moq to mock IPersonRepository. The test purposely fails to show the mock is being used.

---
+++ BusinessApplication.UnitTests/PersonServiceTest.cs
@@ -0,0 +1,30 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BusinessApplication.Core;
+using Moq;
+
+namespace BusinessApplication.UnitTests
+{
+    [TestFixture]
+    public class PersonServiceTest
+    {
+        [Test]
+        public void ShouldBeAbleToGetPersonServiceAndGetPerson()
+        {
+            var expected = new Person { Id = 1, FirstName = "John", LastName = "Doe" };
+            var personRepositoryMock = new Mock<IPersonRespository>();
+            personRepositoryMock
+                .Setup(pr => pr.GetPerson(1))
+                .Returns(new Person { Id = 1, FirstName = "Bob", LastName = "Smith" });
+            var personService = new PersonService(personRepositoryMock.Object);
+            var actual = personService.GetPerson(expected.Id);
+            Assert.AreEqual(expected.Id, actual.Id);
+            Assert.AreEqual(expected.FirstName, actual.FirstName);
+            Assert.AreEqual(expected.LastName, actual.LastName);
+        }
+    }
+}
